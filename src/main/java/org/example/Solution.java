63ef8c5ce881739ab8714c197cb83fc5f61f7547
sidharthdas <sidharthdas.1995@gmail.com>
2026-08-13 13:56:16 +0530

    1297. Maximum Number of Occurrences of a Substring

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -5,22 +5,32 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 public class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
 
         Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
         ListNode temp = head;
 
-        while(head != null) {
-            if(set.contains(head.val)) {
+        while (head != null) {
+            if (set.contains(head.val)) {
                 head = head.next;
-            }else {
+            } else {
                 head = head.next;
             }
         }
--- src/main/java/org/example/TransactionMainClass.java
+++ src/main/java/org/example/TransactionMainClass.java
@@ -1,17 +1,129 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import javax.swing.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TransactionMainClass {
 
     public static void main(String[] args) {
-        System.out.println(invalidTransactions(new String[]{"alice,20,800,mtv","alice,50,1200,mtv"}));
+        //System.out.println(invalidTransactions(new String[]{"alice,20,800,mtv","alice,50,1200,mtv"}));
+
+        //System.out.println(halveArray(new int[]{5, 19, 8, 1}));
+
+        //s = "aababcaab", maxLetters = 2, minSize = 3, maxSize = 4
+        System.out.println(maxFreq("abcde", 2,3,3));
     }
 
-    static class  Transaction{
+    public static int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
+
+        int len = s.length();
+
+        int count = 0;
+
+        List<String> list = new ArrayList<>();
+
+        for(int i = 0; i < len; i++) {
+            Set<Character> set = new HashSet<>();
+            int size = 0;
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int j = i; j < len; j++) {
+
+
+
+                if(size >= minSize && size <= maxSize) {
+                    if(set.size() <= maxLetters) {
+                        list.add(stringBuilder.toString());
+                        count++;
+                    } else  {
+                        break;
+                    }
+
+                }
+                if(size > maxSize) {
+                    break;
+                }
+
+                stringBuilder.append(s.charAt(j));
+                set.add(s.charAt(j));
+                size++;
+            }
+
+            if(size >= minSize && size <= maxSize && set.size() <= maxLetters) {
+                list.add(stringBuilder.toString());
+            }
+        }
+
+        System.out.println(list);
+
+        if(list.isEmpty()) return 0;
+
+        return Math.toIntExact(list.stream()
+                .filter(x -> x.length() >= minSize && x.length() <= maxSize)
+                .collect(Collectors.groupingBy(x -> x, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .findFirst()
+                .get().getValue());
+
+
+        //return count;
+
+    }
+
+    public static int halveArray(int[] nums) {
+
+        PriorityQueue<Double> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        double sum = 0;
+        for (int i : nums) {
+            priorityQueue.offer((double) i);
+            sum += i;
+        }
+
+        int count = 0;
+        double k = 0;
+
+
+        while (sum - k > sum / 2) {
+            double temp = priorityQueue.poll();
+            k += temp / 2;
+            priorityQueue.offer(temp / 2);
+            count++;
+        }
+        return count + 1;
+    }
+
+    public int halveArray1(int[] nums) {
+        double s = 0, k = 0; int i = 0;
+        PriorityQueue<Double> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int x: nums) {
+            pq.offer((double)x);
+            s += x;
+        }
+        while (s - k > s / 2) {
+            double x = pq.poll();
+            k += x / 2;
+            pq.offer(x / 2);
+            i++;
+        }
+        return i;
+    }
+
+    public String repeatLimitedString(String s, int repeatLimit) {
+
+        Map<String, Long> map = Arrays.stream(s.split("")).collect(Collectors
+                .groupingBy(x -> x, Collectors.counting()));
+
+        List<String> list = map.keySet().stream().sorted(Collections.reverseOrder()).toList();
+
+        return null;
+
+
+    }
+
+    static class Transaction {
         String name;
         int time;
         int amount;
@@ -27,13 +139,13 @@ public class TransactionMainClass {
     }
 
 
-    public static  List<String> invalidTransactions(String[] transactions) {
+    public static List<String> invalidTransactions(String[] transactions) {
 
         Map<String, List<Transaction>> map = new HashMap<>();
 
-        for(String l : transactions) {
+        for (String l : transactions) {
             Transaction transaction = new Transaction(l);
-            if(map.containsKey(transaction.name)) {
+            if (map.containsKey(transaction.name)) {
                 map.get(transaction.name).add(transaction);
             } else {
                 List<Transaction> l1 = new ArrayList<>();
@@ -44,10 +156,10 @@ public class TransactionMainClass {
 
         List<String> inValid = new ArrayList<>();
 
-        for(String trans : transactions) {
+        for (String trans : transactions) {
             Transaction transaction = new Transaction(trans);
 
-            if(!isValid(transaction, map.getOrDefault(transaction.name, new ArrayList<>()))) {
+            if (!isValid(transaction, map.getOrDefault(transaction.name, new ArrayList<>()))) {
                 inValid.add(trans);
             }
         }
@@ -62,8 +174,8 @@ public class TransactionMainClass {
             return false;
         }
 
-        for(Transaction transaction1 : list) {
-            if(Math.abs(transaction1.time - transaction.time) <= 60 && !transaction1.city.equals(transaction.city)) {
+        for (Transaction transaction1 : list) {
+            if (Math.abs(transaction1.time - transaction.time) <= 60 && !transaction1.city.equals(transaction.city)) {
                 return false;
             }
         }
